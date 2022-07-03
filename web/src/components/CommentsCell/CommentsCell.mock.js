ce72175f7862b13c7375645bc062f34c3b1c089b
Trond <trond.rensel@bluetree.no>
2022-07-03 16:08:52 +0200

    fines tutorial

---
+++ web/src/components/CommentsCell/CommentsCell.mock.js
@@ -0,0 +1,19 @@
+// Define your own mock data here:
+export const standard = () => ({
+  comments: [
+    {
+      id: 1,
+      name: 'Rob Cameron',
+      body: 'First comment',
+      createdAt: '2020-01-02T12:34:56Z',
+      postId: 1,
+    },
+    {
+      id: 2,
+      name: 'David Price',
+      body: 'Second comment',
+      createdAt: '2020-02-03T23:00:00Z',
+      postId: 1,
+    },
+  ],
+})
